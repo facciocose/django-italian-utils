4ffb9d8014ef42fc5d58ce9b2ff32f3e1aa8e2f9
Luca Marra <luca@facciocose.it>
2014-08-27 01:42:36 +0200

    added setup.py package configuration

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-italian-utils',
+    version='0.1',
+    packages=['italian_utils'],
+    include_package_data=True,
+    license='MIT License',
+    description='Libreria di utility per semplificare la creazione di applicazioni italiane.',
+    url='https://github.com/facciocose/django-italian-utils',
+    author='Luca Marra',
+    author_email='luca@facciocose.it',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
