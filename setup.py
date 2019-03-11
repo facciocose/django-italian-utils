64788adda7203ebc3ddea14424e2b1e899d9d362
fabrixxm <fabrix.xm@gmail.com>
2019-03-11 23:55:42 +0100

    Issue/2 (#3)
    * fix missing packages in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os
-from setuptools import setup
-
+from setuptools import setup, find_packages
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-italian-utils',
-    version='0.3',
-    packages=['italian_utils'],
+    version='0.3.1',
+    packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Libreria di utility per semplificare la creazione di applicazioni italiane.',
