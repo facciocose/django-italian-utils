ebf7499a224372347694a5cdabff798b81bcaf40
Luca Marra <ldm.marra@gmail.com>
2019-03-12 17:40:31 +0100

    use example project in travis for testing

--- example/example/settings.py
+++ example/example/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Aggiunge la cartella in cui è contenuta la libreria al path
-sys.path.append(os.path.dirname(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
