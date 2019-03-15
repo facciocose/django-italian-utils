d9b99dd03fd2d5b82abd5e794e34152288805cd2
Luca Marra <ldm.marra@gmail.com>
2019-03-15 14:58:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='django-italian-utils',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
